2df06516f098210fa32d9de4e01166ce3080e13a
Joshua Ellis <joshua.ellis.r@gmail.com>
2022-01-22 11:06:10 -0500

    Installs and implements node-fetch

--- app.js
+++ app.js
@@ -1,24 +1,53 @@
-const http = require('http');
-const express = require('express');
+const http = require("http");
+const express = require("express");
 const app = express();
+const fetch = require('node-fetch');
 
-const hostname = '127.0.0.1';
+const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer(app);
 
-app.get('/', (req, res) => {
-  res.send('landing')
-})
+app.get("/", (req, res) => {
+  res.send("landing");
+});
 
-app.get('/search', (req, res) => {
-  res.send('search')
-})
+// app.get("/search", (req, res) => {
+//   res.send("search");
+// });
 
+app.get("/search", (req, res) => {
+  const body = `fields id, name, summary; search "destiny 2"; where version_parent = null;`;
 
+  fetch('https://api.igdb.com/v4/games', {
+          method: 'post',
+          body: body,
+          headers: {
+                "Client-ID": "7gs9e0vhvxm1rc38a3323otdgw0g5w",
+                Authorization: "Bearer ow0rr6woggladhfhtur83pfrilx8y5",
+              }
+      })
+      .then(response => response.json())
+      .then(data => console.log(data));
 
+  res.send("search")
 
+  // fetch("https://api.igdb.com/v4/games", {
+  //   method: POST,
+  //   headers: {
+  //     "Client-ID": "7gs9e0vhvxm1rc38a3323otdgw0g5w",
+  //     Authorization: "Bearer ow0rr6woggladhfhtur83pfrilx8y5",
+  //   },
+  //   body: `fields id, name, summary; search "destiny 2"; where version_parent = null;`,
+  // })
+  //   .then((response) => {
+  //     response.json();
+  //   })
+  //   .then((data) => {
+  //     res.send(data);
+  //   });
+});
 
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
