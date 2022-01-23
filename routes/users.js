acc7f36a49f1a60b8f4a50dae64d35240fa44707
Joshua Ellis <joshua.ellis.r@gmail.com>
2022-01-23 10:12:04 -0500

    Refactors /register to search username and email

--- routes/users.js
+++ routes/users.js
@@ -2,26 +2,42 @@ const bcrypt = require("bcrypt");
 const express = require("express");
 const models = require("../models");
 const router = express.Router();
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
 /* POST /api/v1/users/register */
 router.post("/register", function (req, res, next) {
   // If fields are missing
   if (!req.body.username || !req.body.email || !req.body.password) {
     // then send an error back
-    res.status(400).json({error: "please include all required fields"})
+    res.status(400).json({ error: "please include all required fields" });
     return;
   }
 
   // check if email in use
-  models.User.findAll({ where: { email: req.body.email }
-  }).then((users) => {
+  // models.User.findAll({ where: { email: req.body.email }
+  // }).then((users) => {
+  // if yes, send error
+  // if (users.length > 0) {
+  //   res.status(400).json({ error: "email already in use" });
+  //   return;
+  // }
+
+  const checkCredentials = async () => {
+    // check if username or email is in use
+    const username = await models.User.findAll({
+      where: { username: req.body.username },
+    });
+    const email = await models.User.findAll({
+      where: { email: req.body.email },
+    });
     // if yes, send error
-    if (users.length > 0) {
-      res.status(400).json({ error: `${users}email already in use` });
+    if (email.length > 0) {
+      res.status(400).json({ error: "email already in use" });
+      return;
+    } else if (username.length > 0) {
+      res.status(400).json({ error: "username already in use" });
       return;
     }
-
     // if not, create new user
     // hash password
     bcrypt.hash(req.body.password, 10).then((hash) => {
@@ -35,41 +51,40 @@ router.post("/register", function (req, res, next) {
         res.status(201).json(user);
       });
     });
-  });
+  };
+  checkCredentials();
 });
+// });
 
 /* POST /api/v1/users/login */
-router.post('./login', (req, res, next) => {
+router.post("/login", (req, res, next) => {
   // check for required fields
-  if (!req.body.username || !req.body.email || !req.body.password) {
+  if (!req.body.username || !req.body.password) {
     // then send an error back
-    res.status(400).json({error: "please include all required fields"})
+    res.status(400).json({ error: "please include all required fields" });
     return;
   }
-  // check for user with email
-  models.User.findOne({ where: { email: req.body.email }})
-    .then(user => {
-      // if no user, send error
-      if (!user) {
-        res.status(404).json({ error: "could not find the email" });
+  // check for user with username
+  models.User.findOne({ where: { username: req.body.username } }).then((user) => {
+    // if no user, send error
+    if (!user) {
+      res.status(404).json({ error: "could not find the username" });
+      return;
+    }
+    // check password against hash in db
+    bcrypt.compare(req.body.password, user.password).then((match) => {
+      // if no match, send error
+      if (!match) {
+        res.status(400).json({ error: "password incorrect" });
         return;
       }
-      // check password against has in db
-      bcrypt.compare(req.body.password, user.password)
-        .then(match => {
-          // if no match, send error
-          if (!match) {
-            res.status(400).json({ error: "password incorrect" });
-            return;
-          }
-          // log the user in
-          // JWT - JSON Web Tokens
+      // log the user in
+      // JWT - JSON Web Tokens
 
-          // send success response
-          res.json({ success: "logged in" })
-        })
-    })
-})
-
+      // send success response
+      res.json({ success: "logged in" });
+    });
+  });
+});
 
 module.exports = router;
