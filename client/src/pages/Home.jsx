4e723a14633d600475235048f061b2018fa8dd6d
Joshua Ellis <joshua.ellis.r@gmail.com>
2022-01-25 22:32:14 -0500

    Combines IGDB endpoint info in search.js; POST game to DB in Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,53 +1,133 @@
 import axios from "axios";
 import React, { useState } from "react";
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+
 
 const Home = () => {
   const [searchValue, setSearchValue] = useState("");
   const [searchResults, setSearchResults] = useState([]);
+  const [gameName, setGameName] = useState("");
+  const [ranking, setRanking] = useState("");
+  const token = localStorage.getItem("token");
 
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  // Submit function in form
   const handleSearch = (e) => {
     e.preventDefault();
-    axios.post('/api/v1/search', { searchValue })
-    .then((res) => {
-      console.log(res.data)
-      setSearchResults(res.data)
-    })
-    .catch((err) => {alert(err)})
+    axios
+      .post("/api/v1/search", { searchValue })
+      .then((res) => {
+        console.log(res.data);
+        setSearchResults(res.data);
+      })
+      .catch((err) => {
+        alert(err);
+      });
   };
+
+  // Add game to db pt. 1
+  const addGameStart = (e) => {
+    handleShow()
+    setGameName(e.target.id)
+    console.log(gameName)
+  }
+  // Add game to db pt. 2
+  const addGameFinish = (e) => {
+    e.preventDefault();
+    axios({
+      method: "POST",
+      url: "/api/v1/lists/",
+      data: {
+        token,
+        game_name: gameName,
+        ranking
+      }
+    })
+      .then(res => { console.log(res)})
+      .catch(err => alert(err.response.data.error));
+    e.target.innerText = 'Success!';
+    e.target.className = "bg-success text-light"
+  }
 
   return (
     <div>
       <h1>My Videogame List</h1>
       <p>This is the home page.</p>
       <form onSubmit={(e) => handleSearch(e)}>
-          <input
-            onChange={(e) => {setSearchValue(e.target.value);console.log(searchValue)}}
-            value={searchValue}
-            required
-          />
-        <button className="btn btn-primary" type="submit">Search</button>
+        <input
+          onChange={(e) => {
+            setSearchValue(e.target.value);
+            console.log(searchValue);
+          }}
+          value={searchValue}
+          required
+        />
+        <button className="btn btn-primary" type="submit">
+          Search
+        </button>
       </form>
       <ul>
         {searchResults.map((result) => (
           <li key={result.id}>
-            <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="../logo512.png" />
-  <Card.Body>
-    <Card.Title>{result.name}</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
-            
-            
-            </li>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src={result.url} />
+              <Card.Body>
+                <Card.Title>{result.name}</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button onClick={addGameStart} id={result.name} variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </li>
         ))}
       </ul>
+
+      <>
+      {/* <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button> */}
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add game</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form onSubmit={(e) => {addGameFinish(e)}}>
+            <Form.Select onChange={(e) => {setRanking(e.target.value)}} aria-label="Default select example">
+              <option value="0">0</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </Form.Select>
+          <Button variant="primary" type="submit">
+            Save
+          </Button>
+          </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+
     </div>
   );
 };
--- routes/search.js
+++ routes/search.js
@@ -11,6 +11,27 @@ const checkAuth = require("../checkAuth");
 router.post("/", (req, res) => {
   const body = `fields id, name, summary; search "${req.body.searchValue}"; where version_parent = null;`;
 
+  const dataWithImage = (data) => {
+    const newData = data.map((item) => {return item.id});
+    console.log(newData)
+    fetch("https://api.igdb.com/v4/covers", {
+    method: "post",
+    body: `fields url; where game = (${newData});`,
+    headers: {
+      "Client-ID": process.env.CLIENT_ID,
+      Authorization: process.env.AUTHORIZATION,
+    },
+  })
+    .then((response) => response.json())
+    .then((finalData) => {
+      
+      for (let i = 0; i < data.length; i++) {
+        data[i].url = finalData[i].url
+      }
+      res.send(data)
+    });
+  }
+
   fetch("https://api.igdb.com/v4/games", {
     method: "post",
     body: body,
@@ -20,7 +41,8 @@ router.post("/", (req, res) => {
     },
   })
     .then((response) => response.json())
-    .then((data) => res.send(data));
+    .then((data) => dataWithImage(data));
+    // .then((data) => res.send(data))
 
   // res.send("search");
 });
